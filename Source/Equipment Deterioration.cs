a35c20db32bd4f365fedf0e1290ce46a3c3faab5
Rikard Da Costa Stranne <Rikard.DaCostaStranne@enfogroup.com>
2018-11-19 13:40:22 +0100

    Updated with logic to damage weapons upon use. Different calculations for Ranged Weapons and Melee Weapons.

--- Source/Equipment Deterioration.cs
+++ Source/Equipment Deterioration.cs
@@ -11,18 +11,22 @@ namespace Equipment_Deterioration
         {
             HarmonyInstance harmonyInstance = HarmonyInstance.Create("rimworld.limetreesnake.equipmentdeterioration");
             harmonyInstance.Patch(AccessTools.Method(typeof(Pawn_ApparelTracker), "ApparelTrackerTickRare"), new HarmonyMethod(typeof(HarmonyEquipmentDeterioration).GetMethod("ApparelTrackerTickRare_PreFix")), null);
+            harmonyInstance.Patch(AccessTools.Method(typeof(Verb_Shoot), "TryCastShot"), null, new HarmonyMethod(typeof(HarmonyEquipmentDeterioration).GetMethod("TryCastShot_Ranged_PostFix")), null);
+            harmonyInstance.Patch(AccessTools.Method(typeof(Verb_MeleeAttack), "TryCastShot"), null, new HarmonyMethod(typeof(HarmonyEquipmentDeterioration).GetMethod("TryCastShot_Melee_PostFix")), null);
+
+
         }
-        
+
         public static bool ApparelTrackerTickRare_PreFix(Pawn_ApparelTracker __instance)
         {
             int wearoutTick = Traverse.Create(__instance).Field("lastApparelWearoutTick").GetValue<int>();
             int ticksGame = Find.TickManager.TicksGame;
             if (ticksGame - wearoutTick >= 60000)
             {
-                
+
                 for (int i = 0; i < __instance.pawn.equipment.AllEquipmentListForReading.Count; i++)
                 {
-                    int num = GenMath.RoundRandom(SettingsHelper.latest.detoriationRate);
+                    int num = GenMath.RoundRandom(SettingsHelper.latest.detoriationDailyRate);
                     if (num > 0)
                     {
                         __instance.pawn.equipment.AllEquipmentListForReading[0].TakeDamage(new DamageInfo(DamageDefOf.Deterioration, (float)num));
@@ -37,7 +41,61 @@ namespace Equipment_Deterioration
             }
             return true;
         }
+
+        //public static void WarmupComplete_PostFix(Verb __instance)
+        //{
+        //    int num = GenMath.RoundRandom(SettingsHelper.latest.detoriationUsedRate);
+        //    if (num > 0)
+        //    {
+        //        __instance.CasterPawn.equipment.AllEquipmentListForReading[0].TakeDamage(new DamageInfo(DamageDefOf.Deterioration, (float)num));
+        //    }
+        //    if (__instance.CasterPawn.equipment.AllEquipmentListForReading[0].Destroyed && PawnUtility.ShouldSendNotificationAbout(__instance.CasterPawn) && !__instance.CasterPawn.Dead)
+        //    {
+        //        string str = "MessageWornApparelDeterioratedAway".Translate(GenLabel.ThingLabel(__instance.CasterPawn.equipment.AllEquipmentListForReading[0].def, __instance.CasterPawn.equipment.AllEquipmentListForReading[0].Stuff), __instance.CasterPawn);
+        //        str = str.CapitalizeFirst();
+        //        Messages.Message(str, __instance.CasterPawn, MessageTypeDefOf.NegativeEvent);
+        //    }
+        //}
+
+        public static void TryCastShot_Melee_PostFix(Verb_MeleeAttack __instance, bool __result)
+        {
+            if (!__instance.CasterPawn.AnimalOrWildMan() && __instance.CasterPawn.equipment.Primary != null)
+            {
+                if (!__result)
+                    return;
+                int num = GenMath.RoundRandom(SettingsHelper.latest.detoriationMeleeUsedRate);
+                if (num > 0)
+                {
+                    __instance.CasterPawn.equipment.Primary.TakeDamage(new DamageInfo(DamageDefOf.Deterioration, (float)num));
+                }
+                if (__instance.CasterPawn.equipment.Primary.Destroyed && PawnUtility.ShouldSendNotificationAbout(__instance.CasterPawn) && !__instance.CasterPawn.Dead)
+                {
+                    string str = "MessageWornApparelDeterioratedAway".Translate(GenLabel.ThingLabel(__instance.CasterPawn.equipment.Primary.def, __instance.CasterPawn.equipment.Primary.Stuff), __instance.CasterPawn);
+                    str = str.CapitalizeFirst();
+                    Messages.Message(str, __instance.CasterPawn, MessageTypeDefOf.NegativeEvent);
+                }
+            }
+        }
+        public static void TryCastShot_Ranged_PostFix(Verb_Shoot __instance)
+        {
+            if (!__instance.CasterPawn.AnimalOrWildMan() && __instance.CasterPawn.equipment.Primary != null)
+            {
+                int num = GenMath.RoundRandom(SettingsHelper.latest.detoriationRangedUsedRate);
+                if (num > 0)
+                {
+                    __instance.CasterPawn.equipment.Primary.TakeDamage(new DamageInfo(DamageDefOf.Deterioration, (float)num));
+                }
+                if (__instance.CasterPawn.equipment.Primary.Destroyed && PawnUtility.ShouldSendNotificationAbout(__instance.CasterPawn) && !__instance.CasterPawn.Dead)
+                {
+                    string str = "MessageWornApparelDeterioratedAway".Translate(GenLabel.ThingLabel(__instance.CasterPawn.equipment.Primary.def, __instance.CasterPawn.equipment.Primary.Stuff), __instance.CasterPawn);
+                    str = str.CapitalizeFirst();
+                    Messages.Message(str, __instance.CasterPawn, MessageTypeDefOf.NegativeEvent);
+                }
+            }
+        }
     }
 
 
-}   
+
+
+}
--- Source/ModSettings.cs
+++ Source/ModSettings.cs
@@ -5,14 +5,20 @@ namespace Equipment_Deterioration
 {
     internal class DeteriorationSettings : ModSettings
     {
-        public float detoriationRate = base_detoriationRate;
+        public float detoriationDailyRate = base_detoriationDailyRate;
+        public float detoriationRangedUsedRate = base_detoriationRangedUsedRate;
+        public float detoriationMeleeUsedRate = base_detoriationMeleeUsedRate;
 
-        private static readonly float base_detoriationRate = 0.25f;
+        private static readonly float base_detoriationDailyRate = 0.25f;
+        private static readonly float base_detoriationRangedUsedRate = 0.05f;
+        private static readonly float base_detoriationMeleeUsedRate = 0.15f;
 
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref detoriationRate, "DetoriationRate", base_detoriationRate);
+            Scribe_Values.Look(ref detoriationDailyRate, "DetoriationDailyRate", base_detoriationDailyRate);
+            Scribe_Values.Look(ref detoriationRangedUsedRate, "DetoriationUsedRate", base_detoriationRangedUsedRate);
+            Scribe_Values.Look(ref detoriationMeleeUsedRate, "DetoriationUsedRate", base_detoriationMeleeUsedRate);
         }
     }
 
@@ -37,8 +43,17 @@ namespace Equipment_Deterioration
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
-            Rect rect = inRect.TopHalf();
-            deteriorationSettings.detoriationRate = Widgets.HorizontalSlider(rect.TopHalf().ContractedBy(5f), deteriorationSettings.detoriationRate, 0f, 1f, middleAlignment: true, "Deterioration Rate " + deteriorationSettings.detoriationRate * 100f + "\n Determines the chance of which items carried detoriates by 1 point each day.", "0%", "100%");
+            Rect rectTop = inRect.TopHalf();
+            Rect rectBot = inRect.BottomHalf();
+
+            Rect rect1 = rectTop.TopHalf();
+            Rect rect2 = rectTop.BottomHalf();
+            Rect rect3 = rectBot.TopHalf();
+            Rect rect4 = rectBot.BottomHalf();
+
+            deteriorationSettings.detoriationDailyRate = Widgets.HorizontalSlider(rect1.TopHalf().ContractedBy(5f), deteriorationSettings.detoriationDailyRate, 0f, 1f, middleAlignment: true, "Deterioration Daily Rate " + deteriorationSettings.detoriationDailyRate * 100f + "\n Determines the chance of which items carried detoriates by 1 point each day.", "0%", "100%");
+            deteriorationSettings.detoriationRangedUsedRate = Widgets.HorizontalSlider(rect1.BottomHalf().ContractedBy(5f), deteriorationSettings.detoriationRangedUsedRate, 0f, 1f, middleAlignment: true, "Deterioration Ranged Use Rate " + deteriorationSettings.detoriationRangedUsedRate * 100f + "\n Determines the chance of which Ranged weapons used detoriates by 1 for each Shot.", "0%", "100%");
+            deteriorationSettings.detoriationMeleeUsedRate = Widgets.HorizontalSlider(rect2.TopHalf().ContractedBy(5f), deteriorationSettings.detoriationMeleeUsedRate, 0f, 1f, middleAlignment: true, "Deterioration Melee Use Rate " + deteriorationSettings.detoriationMeleeUsedRate * 100f + "\n Determines the chance of which Melee weapons used detoriates by 1 for each Hit.", "0%", "100%");
 
             deteriorationSettings.Write();
         }
